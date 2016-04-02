748858edb25214d4f240646ea889f192d7430f0b
Răzvan <razvan-mccrafter@outlook.com>
2016-04-02 10:58:06 +0300

    Still working on Graphics

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,11 +1,110 @@
 #include "Graphics.h"
 
+#include "Window.h"
 
-Graphics::Graphics()
+#ifndef _DELETEMACROS_H
+	#include "deletemacros.h"
+#endif
+
+//Struct
+GraphicsData::GraphicsData(Window*wnd)
+: SystemData(SystemType::Sys_Graphics)
+, pWnd(wnd)
+{}
+
+//Class Graphics
+Graphics::Graphics(const GraphicsData& data)
+: System(data)
+, m_pRenderTarget(nullptr)
+, m_pD2DFactory(nullptr)
+, m_pColorBrush(nullptr)
+, m_pImageFactory(nullptr)
+
+, m_pWindow(data.pWnd)
+{
+}
+Graphics::~Graphics()
 {
 }
 
-
-Graphics::~Graphics()
+HRESULT Graphics::OnResize(UINT width, UINT height)
 {
+	if (m_pRenderTarget)
+		return m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+	else return S_FALSE;
+}
+
+bool Graphics::Initialize()
+{
+	System::Initialize();
+
+	HRESULT hr = CreateDeviceIndependentResources();
+	if (FAILED(hr))
+		return false;
+	if (FAILED(hr))
+		return false;
+
+	//Logger::Log(_T("Graphics is initialized"), LOGTYPE_INFO, false);
+	return true;
+}
+bool Graphics::ShutDown()
+{
+	System::ShutDown();
+
+	SafeRelease(m_pD2DFactory);
+	SafeRelease(m_pColorBrush);
+	SafeRelease(m_pImageFactory);
+	SafeRelease(m_pRenderTarget);
+
+	return true;
+}
+
+//Private Methods
+HRESULT Graphics::CreateDeviceIndependentResources()
+{
+	HRESULT hr = S_OK;
+	
+	//Create a Direct2D factory
+	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+	hr = CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER, IID_IWICImagingFactory, (LPVOID*)&m_pImageFactory);
+}
+
+HRESULT Graphics::CreateDeviceDependentResources()
+{
+	HRESULT hr = S_OK;
+	if (!m_pRenderTarget)
+	{
+		//Get the handle to the window
+		HWND hWnd = m_pWindow->GetWndHandle();
+
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+
+		//Create a Direct2D render target
+		hr = m_pD2DFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(hWnd, size), &m_pRenderTarget);
+		if (FAILED(hr))
+			return hr;
+		//Create a brush
+		hr = m_pRenderTarget->CreateSolidColorBrush((D2D1::ColorF) D2D1::ColorF::Black, m_pColorBrush);
+		if (FAILED(hr))
+			return hr;
+	}
+	return hr;
+}
+
+void Graphics::DiscardDeviceResources()
+{
+	SafeRelease(m_pRenderTarget);
+}
+
+void Graphics::BeginDraw()
+{
+	m_pRenderTarget->BeginDraw();
+	m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+}
+
+HRESULT Graphics::EndDraw()
+{
+	return m_pRenderTarget->EndDraw();
 }
--- Graphics.h
+++ Graphics.h
@@ -1,10 +1,74 @@
-#pragma once
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
+
+//C Runtime Header Files
+
+//Additional Header Files
 #include "System.h"
-class Graphics :
-	public System
+/*#ifndef _LOGGER_H
+	#include "Logger.h"
+#endif*/
+#ifndef _2DUTILL_H
+	#include "d2dutill.h"
+#endif
+
+//Foward Declaration
+class Window;
+
+//Struct
+struct GraphicsData : public SystemData
 {
 public:
-	Graphics();
-	virtual ~Graphics();
+	GraphicsData(Window* wnd = nullptr);
+
+	Window* pWnd;
 };
+
+class Graphics : public System
+{
+	friend class Engine;
+public:
+	//Methods
+	HRESULT OnResize(UINT width, UINT height);
+
+	ID2D1HwndRenderTarget* GetRenderTarget();
+	IWICImagingFactory* GetImageFactory();
+	ID2D1SolidColorBrush* GetColorBrush();
+	ID2D1Factory* GetD2DFactory();
+
+protected:
+	//Methods
+	//Constructor & Destructor
+	Graphics(const GraphicsData& data);
+	virtual ~Graphics();
+
+	//Copy Constructor & Default Assigment Operator
+	Graphics(const Graphics& other);
+	Graphics& operator=(const Graphics& tref);
+
+	bool Initialize();
+	bool ShutDown();
+
+private:
+	//Methods
+	//void* operator new(size_t size);
+	//void operator delete(void* pdelete);
+
+	HRESULT CreateDeviceIndependentResources();
+	HRESULT CreateDeviceDependentResources();
+
+	void DiscardDeviceResources();
+
+	void BeginDraw();
+	HRESULT EndDraw();
+
+	//Members
+	ID2D1HwndRenderTarget* m_pRenderTarget;
+	IWICImagingFactory* m_pImageFactory;
+	ID2D1SolidColorBrush* m_pColorBrush;
+	ID2D1Factory* m_pD2DFactory;
+
+	Window* m_pWindow;
+};
+#endif
 
